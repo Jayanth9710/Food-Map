cfaa5125184a9a51af7507606e18979d8990492c
Jayanth9710 <68297123+Jayanth9710@users.noreply.github.com>
2021-02-25 19:17:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var zoom=5;
 	var latstart=22.76;
 	var longstart=78;
 	var markers;
-
-	var map = L.map('map').setView([latstart, longstart], zoom);
+	var map = L.map('map').setView([latstart,longstart],zoom);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
