c779b85429a5c18f8bfc967caaaf3026007423f9
Jayanth9710 <68297123+Jayanth9710@users.noreply.github.com>
2021-02-25 18:59:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([21.765882,78.338100], 1);
+var map = L.map('map').setView([21.765882,78.338100],5);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
