651e60c4c9a5bc5c54829fbb5c2e64e6c492fe28
Jayanth9710 <68297123+Jayanth9710@users.noreply.github.com>
2021-02-25 18:51:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,5 +3,3 @@ var map = L.map('map').setView([21.765882, 78.338100], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
-
