832cc489314d0e0135069db3a33d9d2f58b0919e
Jayanth9710 <68297123+Jayanth9710@users.noreply.github.com>
2021-02-25 18:45:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([21.765882,78.338100 ]).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
