3c747d6aa1e5adaf8629a43e071241bb4a7aa9a5
Jayanth9710 <68297123+Jayanth9710@users.noreply.github.com>
2021-02-25 19:06:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var map = L.map('map').setView([21.765882,78.338100],20);
-            L.map('map').zoom(0);
+            L.map('map').setZoom(0);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
