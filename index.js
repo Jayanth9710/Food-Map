ed5a787198ed5ea489f1b87227a112513ba40c4e
Jayanth9710 <68297123+Jayanth9710@users.noreply.github.com>
2021-02-26 12:35:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ var zoom=5;
 	var longstart=78;
 	var markers;
 	var map = L.map('map').setView([latstart,longstart],zoom);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+var googleLayer = new L.Google('HYBRID');
+	map.addLayer(googleLayer,true);
